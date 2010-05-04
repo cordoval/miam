4a27f116443eaa4c122956f77ab9646c051a34b0
ornicar <thibault.duplessis@gmail.com>
2010-05-04 17:58:53 +0200

    Add fixtures

--- src/Bundle/MiamBundle/data/fixtures/doctrine/data.php
+++ src/Bundle/MiamBundle/data/fixtures/doctrine/data.php
@@ -4,7 +4,17 @@ use Bundle\MiamBundle\Entities\Story;
 
 $em = $this->container->getDoctrine_ORM_DefaultEntityManagerService();
 
-$story = new Story();
-$story->setName('Smoke in the water');
-$story->setPriority(1);
-$story->setBody("Hey there where ya goin', not exactly knowin', who says you have to call just one place home. He's goin' everywhere, B.J. McKay and his best friend Bear.");
+$story1 = new Story();
+$story1->setName('Smoke in the water');
+$story1->setPriority(1);
+$story1->setBody("Hey there where ya goin', not exactly knowin', who says you have to call just one place home. He's goin' everywhere, B.J. McKay and his best friend Bear.");
+
+$story2 = new Story();
+$story2->setName('Danse on a volcano');
+$story2->setPriority(2);
+$story2->setBody("Ulysses, Ulysses - Soaring through all the galaxies. In search of Earth, flying in to the night.");
+
+$story3 = new Story();
+$story3->setName('Saucerful of secrets');
+$story3->setPriority(3);
+$story3->setBody("I never spend much time in school but I taught ladies plenty. It's true I hire my body out for pay, hey hey. I've gotten burned over Cheryl Tiegs, blown up for Raquel Welch.");
