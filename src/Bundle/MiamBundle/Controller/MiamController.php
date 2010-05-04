409daf10ab4802790bfd44212421a7333479c752
Matthieu Bontemps <matthieu.bontemps@gmail.com>
2010-05-04 18:16:33 +0200

    Add a simple index with all stories

--- src/Bundle/MiamBundle/Controller/MiamController.php
+++ src/Bundle/MiamBundle/Controller/MiamController.php
@@ -2,12 +2,22 @@
 
 namespace Bundle\MiamBundle\Controller;
 
-use Symfony\Framework\WebBundle\Controller;
+use Symfony\Framework\DoctrineBundle\Controller\DoctrineController as Controller;
 
 class MiamController extends Controller
 {
   public function indexAction()
   {
-    return $this->render('MiamBundle:Miam:index');
+    $em = $this->getEntityManager();
+    $qb = $em->createQueryBuilder()
+      ->select('s')
+      ->from('MiamBundle:Story', 's');
+
+    $query = $qb->getQuery();
+    $stories = $query->execute();
+
+    return $this->render('MiamBundle:Miam:index', array(
+      'stories' => $stories,
+    ));
   }
 }
--- src/Bundle/MiamBundle/Resources/views/Miam/index.php
+++ src/Bundle/MiamBundle/Resources/views/Miam/index.php
@@ -1,3 +1,9 @@
 <?php $view->extend('MiamBundle::layout') ?>
 
 Miam index!
+
+<?php foreach($stories as $story): ?>
+<div class="story">
+  <h2><?php echo $story->getName() ?></h2>
+</div>
+<?php endforeach; ?>
