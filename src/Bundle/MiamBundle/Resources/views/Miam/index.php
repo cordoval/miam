4bf9d24673a74d6c7e90fd8095d5449d010fa8d0
Matthieu Bontemps <matthieu.bontemps@gmail.com>
2010-05-14 18:02:05 +0200

    Add a breadcrumb and a edit link

--- src/Bundle/MiamBundle/Resources/views/Miam/index.php
+++ src/Bundle/MiamBundle/Resources/views/Miam/index.php
@@ -7,9 +7,8 @@
     <ol class="stories">
         <?php foreach ($stories as $story): ?>
             <li class="story" id="story_<?php echo $story->getId() ?>">
-                <a href="<?php echo $view->router->generate('story', array('id' => $story->getId())) ?>">
-                    #<?php echo $story->getId() ?> − <?php echo $story->getName() ?>
-                </a>
+                <a href="<?php echo $view->router->generate('story', array('id' => $story->getId())) ?>">#<?php echo $story->getId() ?> − <?php echo $story->getName() ?></a>
+                <span class="editLink"><a href="<?php echo $view->router->generate('story_edit', array('id' => $story->getId())) ?>">modifier</a></span>
             </li>
         <?php endforeach; ?>
     </ol>
