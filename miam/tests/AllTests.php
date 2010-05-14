2ad0d14e41fd0f283128dc7afecf3f580dc89c9c
Matthieu Bontemps <matthieu.bontemps@gmail.com>
2010-05-14 13:44:28 +0200

    Add a functional test for 'show a story' and 'edit a story' (failing)

--- miam/tests/AllTests.php
+++ miam/tests/AllTests.php
@@ -4,6 +4,8 @@ namespace Bundle\MiamBundle\Tests;
 
 require_once 'PHPUnit/Framework.php';
 require_once __DIR__.'/Functional/createAStoryTest.php';
+require_once __DIR__.'/Functional/showAStoryTest.php';
+require_once __DIR__.'/Functional/editAStoryTest.php';
 
 class AllTests
 {
@@ -11,7 +13,9 @@ class AllTests
   {
     $suite = new \PHPUnit_Framework_TestSuite('MiamBundle_Functional');
 
+    $suite->addTestSuite('Miam\Tests\Functional\showAStoryTest');
     $suite->addTestSuite('Miam\Tests\Functional\createAStoryTest');
+    $suite->addTestSuite('Miam\Tests\Functional\editAStoryTest');
 
     return $suite;
   }
---
+++ miam/tests/Functional/editAStoryTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Miam\Tests\Functional;
+
+use Bundle\PHPUnitBundle\Functional;
+
+class editAStoryTest extends \WebTestCase
+{
+    
+    public function testEditAStoryShowsItOnTheBacklog()
+    {
+        $crawler = $this->client->request('GET', '/');
+        $this->client->click($crawler->selectLink('#1 − Smoke in the water')->link());
+        
+        $crawler = $this->client->getCrawler();
+        $this->client->click($crawler->selectLink('Modifier')->link());
+
+        $crawler = $this->client->getCrawler();
+        $this->addRequestTester();
+        $this->client->assertRequestParameter('_route', 'story_edit');
+        $this->client->assertRequestParameter('_controller', 'MiamBundle:Miam:edit');
+
+        $form = $crawler->selectButton('Valider')->form();
+        $this->client->submit($form, array(
+            'story[name]' => 'Edited you',
+            'story[body]' => 'lorem',
+            'story[points]' => '12'
+        ));
+        $this->client->followRedirect();
+
+        $this->addRequestTester();
+        $this->client->assertRequestParameter('_route', 'backlog');
+        
+        $this->addResponseTester();
+        $this->client->assertResponseRegExp('/Edited you/');
+    }
+
+
+}
