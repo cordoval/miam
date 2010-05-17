e4bd9ed052ef84f2c580e75c8c871c28960669cf
Matthieu Bontemps <matthieu.bontemps@gmail.com>
2010-05-17 09:36:57 +0200

    Add points in the backlogs

--- src/Bundle/MiamBundle/Resources/views/Miam/index.php
+++ src/Bundle/MiamBundle/Resources/views/Miam/index.php
@@ -8,8 +8,9 @@
         <?php foreach ($stories as $story): ?>
             <li class="story" id="story_<?php echo $story->getId() ?>">
                 <a href="<?php echo $view->router->generate('story', array('id' => $story->getId())) ?>">#<?php echo $story->getId() ?> − <?php echo $story->getName() ?></a>
+                <span class="story_points"><?php echo $story->getPoints() ? $story->getPoints() : '?' ?></span>
                 <span class="editLink"><a href="<?php echo $view->router->generate('story_edit', array('id' => $story->getId())) ?>">modifier</a></span>
             </li>
-        <?php endforeach; ?>
+        <?php endforeach ?>
     </ol>
 </div>
