2d156f2395f6cca0ee8c69a9ca3eb547c3199dd7
Matthieu Bontemps <matthieu.bontemps@gmail.com>
2010-05-21 11:47:17 +0200

    Introduce sprints

---
+++ src/Bundle/MiamBundle/Entities/Sprint.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Bundle\MiamBundle\Entities;
+
+/**
+ * @Entity(repositoryClass="Bundle\MiamBundle\Entities\SprintRepository")
+ * @Table(name="miam_sprint")
+ */
+class Sprint
+{
+    /**
+    * @OneToMany(targetEntity="Story", mappedBy="sprint")
+    */
+    protected $stories;
+    
+    /**
+     * @Column(name="starts_at", type="date")
+     */
+    protected $startsAt;
+
+    /**
+     * @Column(name="endsAt", type="date")
+     */
+    protected $endsAt;
+
+    /**
+     * @Column(name="is_current", type="boolean", nullable=false)
+     */
+    protected $isCurrent;
+
+
+    /**
+     * @Column(name="id", type="integer")
+     * @Id
+     * @GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        $this->isCurrent = true;
+    }
+
+    public function getIsCurrent()
+    {
+        return $this->isCurrent;
+    }
+
+    public function setIsCurrent($isCurrent)
+    {
+        $this->isCurrent = $isCurrent;
+    }
+    
+    public function getStartsAt()
+    {
+        return $this->startsAt;
+    }
+
+    public function setStartsAt($startsAt)
+    {
+        $this->startsAt = $startsAt;
+    }
+    
+    public function getEndsAt()
+    {
+        return $this->endsAt;
+    }
+
+    public function setEndsAt($endsAt)
+    {
+        $this->endsAt = $endsAt;
+    }
+    
+    public function getStories()
+    {
+        return $this->stories;
+    }
+
+    /**
+     * Return an array version of a Story's properties
+     * @return array
+     */
+    public function toArray()
+    {
+        return array(
+            'id' => $this->getId(),
+            'starts_at' => $this->getStartsAt(),
+            'ends_at' => $this->getEndsAt(),
+            'is_current' => $this->getIsCurrent()
+        );
+    }
+
+    /**
+     * Get id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function __toString()
+    {
+        return 'Sprint du ' . $this->getStartsAt()->format('d/m/Y') . ' au ' . $this->getEndsAt()->format('d/m/Y');
+    }
+    
+}
