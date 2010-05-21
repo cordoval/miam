d5e51a4ba3d648c7e07ed46dd2706d2e9320aec1
ornicar <thibault.duplessis@gmail.com>
2010-05-21 12:01:31 +0200

    Add sprint controller

---
+++ src/Bundle/MiamBundle/Controller/SprintController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Bundle\MiamBundle\Controller;
+
+use Symfony\Framework\DoctrineBundle\Controller\DoctrineController as Controller;
+use Symfony\Components\HttpKernel\Exception\NotFoundHttpException;
+use Bundle\MiamBundle\Entities\Sprint;
+use Bundle\MiamBundle\Renderer\SprintRenderer;
+use Bundle\MiamBundle\Form\SprintForm;
+
+class SprintController extends Controller
+{
+}
