d7aa25cb45ee46f30c9620220ee089aff5edbf28
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-05-31 22:53:51 +0200

    Restore story form messages

--- src/Bundle/MiamBundle/Form/StoryForm.php
+++ src/Bundle/MiamBundle/Form/StoryForm.php
@@ -19,8 +19,8 @@ use Symfony\Components\Validator\Mapping\ClassMetadata;
 use Symfony\Components\Validator\Mapping\Loader\LoaderChain;
 use Symfony\Components\Validator\Mapping\Loader\StaticMethodLoader;
 use Symfony\Components\Validator\Mapping\Loader\XmlFileLoader;
-use Bundle\MiamBundle\Validator\BlackHoleMessageInterpolator;
 use Symfony\Foundation\UniversalClassLoader;
+use Bundle\MiamBundle\Validator\NoValidationXliffMessageInterpolator;
 
 use Bundle\MiamBundle\Entities\Project;
 
@@ -64,7 +64,7 @@ class StoryForm extends DoctrineForm
       new XmlFileLoader($validationFile)
     )));
     $validatorFactory = new ConstraintValidatorFactory();
-    $messageInterpolator = new BlackHoleMessageInterpolator($messageFile);
+    $messageInterpolator = new NoValidationXliffMessageInterpolator($messageFile);
     $validator = new Validator($metadataFactory, $validatorFactory, $messageInterpolator);
 
     return $validator;
