3fe078af96e3a87c01b31ee00d08c0f5b80eab42
Matthieu Bontemps <matthieu.bontemps@gmail.com>
2010-05-31 18:53:59 +0200

    Use --checksum in rsync

--- miam/deploy.sh
+++ miam/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Usage: ./deploy.sh username@server
-rsync --archive --force --delete --progress --compress --exclude-from=miam/config/rsync_exclude.txt -e ssh ./ $1:site/
+rsync --archive --force --delete --progress --compress --checksum --exclude-from=miam/config/rsync_exclude.txt -e ssh ./ $1:site/
