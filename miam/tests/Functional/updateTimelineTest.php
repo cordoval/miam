bcf2062545f9c8bdc10641d3689140fe121a3df3
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-06-01 20:10:11 +0200

    Add functional test for story estimation timeline

--- miam/tests/Functional/updateTimelineTest.php
+++ miam/tests/Functional/updateTimelineTest.php
@@ -18,8 +18,38 @@ class UpdateTimelineTest extends \WebTestCase
         ));
         
         $crawler = $this->client->request('GET', '/timeline');
+
         $this->addResponseTester();
         $this->client->assertResponseSelectEquals('.tentry .tentry_user', array('_text'), array('thib', 'thib', 'matt'));
         $this->client->assertResponseSelectEquals('.tentry ', array('_text'), array(sprintf('thib a créé bluk [Miam] à %s', date('H:i')), 'thib a commenté Danse on a volcano [Miam] à 16:02', 'matt a créé Smoke in the water [Miam] à 15:00'));
+
+        $this->client->assertResponseSelectEquals('.tentry.first .tentry_user', array('_text'), array('thib'));
+        $this->client->assertResponseSelectEquals('.tentry.first', array('_text'), array(sprintf('thib a créé bluk [Miam] à %s', date('H:i'))));
+    }
+    
+    public function testEstimateStoryUpdatesTimeline()
+    {
+        $this->login('thib', 'changeme');
+        $crawler = $this->client->request('GET', '/story/new');
+
+        $form = $crawler->selectButton('Valider')->form();
+        $this->client->submit($form, array(
+            'story[name]' => 'bluk'
+        ));
+        
+        $crawler = $this->client->request('GET', '/');
+        
+        $crawler = $this->client->click($crawler->selectLink('bluk')->link());
+        $crawler = $this->client->click($crawler->selectLink('Modifier')->link());
+        $form = $crawler->selectButton('Valider')->form();
+        $this->client->submit($form, array(
+            'story[name]' => 'bluk',
+            'story[points]' => 100
+        ));
+        $crawler = $this->client->request('GET', '/timeline');
+
+        $this->addResponseTester();
+        $this->client->assertResponseSelectEquals('.tentry.first .tentry_user', array('_text'), array('thib'));
+        $this->client->assertResponseSelectEquals('.tentry.first', array('_text'), array(sprintf('thib a estimé bluk [Miam] à 100 points à %s', date('H:i'))));
     }
 }
--- src/Bundle/MiamBundle/Resources/views/Timeline/list.php
+++ src/Bundle/MiamBundle/Resources/views/Timeline/list.php
@@ -1,6 +1,6 @@
 <ol class="timeline">
-    <?php foreach ($timeline as $entry): ?>
-        <li class="tentry" id="tentry_<?php echo $entry->getId() ?>"><?php echo strtr(
+    <?php foreach ($timeline as $index => $entry): ?>
+    <li class="tentry<?php !$index && print ' first' ?>" id="tentry_<?php echo $entry->getId() ?>"><?php echo strtr(
                 '<b class="tentry_user">{user}</b> ' . $entry->renderAction() . ' {date}',
                 array(
               '{story}' => '<a href="' . $view->router->generate('story', array('id' => $entry->getStory()->getId())) . '">'. $entry->getStory()->getName() . ' [' . $entry->getStory()->getProject()->getName() . ']</a>',
