8b5253eeeeccc8a4aa0a2222cb602ab67bfd088b
ornicar <thibault.duplessis@gmail.com>
2010-06-01 15:24:09 +0200

    Add timeline functional tests

--- miam/tests/Functional/updateTimelineTest.php
+++ miam/tests/Functional/updateTimelineTest.php
@@ -7,19 +7,36 @@ use Bundle\PHPUnitBundle\Functional;
 class UpdateTimelineTest extends \WebTestCase
 {
     
-    public function testCreateProjectUpdatesTimeline()
+    public function testCreateStoryUpdatesTimeline()
     {
-        $crawler = $this->client->request('GET', '/project/new');
+        $this->login('thib');
+        $crawler = $this->client->request('GET', '/story/new');
 
         $form = $crawler->selectButton('Valider')->form();
         $this->client->submit($form, array(
-            'project[name]' => 'bluk',
-            'project[color]' => '#00FF00',
+            'story[name]' => 'bluk'
         ));
         
         $crawler = $this->client->request('GET', '/timeline');
-        $this->client->assertResponseSelectEquals('.tentry .tentry_user', array('_text'), array('thib', 'matt'));
-        $this->client->assertResponseSelectEquals('.tentry ', array('_text'), array('thib a commenté Danse on a volcano [Miam] à 16:02', 'matt a créé Smoke in the water [Miam] à 15:00'));
+        $this->addResponseTester();
+        $this->client->assertResponseSelectEquals('.tentry .tentry_user', array('_text'), array('thib', 'thib', 'matt'));
+        $this->client->assertResponseSelectEquals('.tentry ', array('_text'), array(sprintf('thib a créé bluk [Miam] à %s', date('H:i')), 'thib a commenté Danse on a volcano [Miam] à 16:02', 'matt a créé Smoke in the water [Miam] à 15:00'));
+    }
+
+    protected function login($username)
+    {
+        $crawler = $this->client->request('GET', '/user/login');
+
+        $form = $crawler->selectButton('Log in')->form();
+        $this->client->submit($form, array(
+            'username' => $username,
+            'password' => 'changeme'
+        ));
+
+
+        $this->client->followRedirect();
+        $this->addResponseTester();
+        $this->client->assertResponseSelectEquals('div.auth span.username', array('_text'), array($username));
     }
 
 }
