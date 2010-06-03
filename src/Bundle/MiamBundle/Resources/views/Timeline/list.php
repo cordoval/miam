0479dd8a49c4c0248d8ff85de441fb9659240490
ornicar <thibault.duplessis@gmail.com>
2010-06-03 11:12:16 +0200

    Minor timeline fix

--- src/Bundle/MiamBundle/Resources/views/Timeline/list.php
+++ src/Bundle/MiamBundle/Resources/views/Timeline/list.php
@@ -1,23 +1,24 @@
 <ol class="timeline">
     <?php foreach ($timeline as $index => $entry): ?>
+    <?php $story = $entry->getStory() ?>
     <li class="tentry<?php !$index && print ' first' ?> action_<?php echo $entry->getAction() ?> clearfix" id="tentry_<?php echo $entry->getId() ?>">
 
         <img class="gravatar" src="<?php echo Bundle\GravatarBundle\Api::getUrl($emails[$entry->getUser()->getUsername()], array('size' => 40)) ?>" width="40" height="40"/>
         <div class="title">
         <?php
-        $storyUrl = $view->router->generate('story', array('id' => $entry->getStory()->getId()));
+        $storyUrl = $view->router->generate('story', array('id' => $story->getId()));
         echo strtr(
             '<strong class="tentry_action"><span class="tentry_user">{user}</span> ' . $entry->renderAction() . ' </strong><span class="date">{date}</span>',
             array(
-                '{story}' => '<a href="' . $storyUrl . '">#'. $entry->getStory()->getId().'</a>',
+                '{story}' => '<a href="' . $storyUrl . '">#'. $story->getId().'</a>',
                 '{user}' => $entry->getUser()->getUsername(),
                 '{date}' => '<span class="tentry_ago">à ' . $entry->getCreatedAt()->format('H:i') . '</span>',
-                '{points}' => $entry->getStory()->getPoints()
+                '{points}' => $story->getPoints() ? $story->getPoints() : '?'
         )) ?>
         </div>
         <div class="details">
-            <a href="<?php echo $storyUrl ?>"><?php echo $entry->getStory()->getName() ?></a>
-            <span class="points"><?php echo $entry->getStory()->getPoints() ?></span>
+            <a href="<?php echo $storyUrl ?>"><?php echo $story->getName() ?></a>
+            <span class="points"><?php echo $story->getPoints() ? $story->getPoints() : '?' ?></span>
         </div>
     </li>
     <?php endforeach ?>
