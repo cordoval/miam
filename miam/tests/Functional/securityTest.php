f9d564f8682cc6de2c7a7b41f7f79929d74f5f45
ornicar <thibault.duplessis@gmail.com>
2010-06-03 11:56:31 +0200

    Add security tests

--- miam/tests/Functional/securityTest.php
+++ miam/tests/Functional/securityTest.php
@@ -7,13 +7,20 @@ use Bundle\PHPUnitBundle\Functional;
 class securityTest extends \WebTestCase
 {
     
-    //public function testAnonymousUserMustLogin()
-    //{
-        //$crawler = $this->client->request('GET', '/');
+    public function testAnonymousUserMustLogin()
+    {
+        $crawler = $this->client->request('GET', '/');
 
-        //$this->addRequestTester();
-        //$this->client->assertRequestParameter('_route', 'login');
-        //$this->client->assertRequestParameter('_controller', 'DoctrineUserBundle:Auth:login');
-   //}
+        $this->addResponseTester();
+        $this->client->assertResponseRegExp('/Tu n\'es pas connecté/');
+    }
+
+    public function testLogin()
+    {
+        $this->login('thib', 'changeme');
+
+        $this->addResponseTester();
+        $this->client->assertResponseRegExp('/Hello, thib!/');
+    }
 
 }
