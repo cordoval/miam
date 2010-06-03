cec3c8eec2928c98d18c850ec88bbf00bf93f625
ornicar <thibault.duplessis@gmail.com>
2010-06-03 12:07:30 +0200

    Fix coding standarts

--- src/Bundle/MiamBundle/Form/ProjectForm.php
+++ src/Bundle/MiamBundle/Form/ProjectForm.php
@@ -16,27 +16,27 @@ use Bundle\MiamBundle\Entities\Project;
 
 class ProjectForm extends Form
 {
-  public function __construct(Project $object, array $options = array())
-  {
-    $this->addOption('message_file');
-    $this->addOption('validation_file'); 
-    $validator = $this->createValidator($options['message_file'], $options['validation_file']);
-    parent::__construct('project', $object, $validator, $options);
+    public function __construct(Project $object, array $options = array())
+    {
+        $this->addOption('message_file');
+        $this->addOption('validation_file'); 
+        $validator = $this->createValidator($options['message_file'], $options['validation_file']);
+        parent::__construct('project', $object, $validator, $options);
 
-    $this->add(new TextField('name'));
-    $this->add(new TextField('color'));
-  } 
+        $this->add(new TextField('name'));
+        $this->add(new TextField('color'));
+    } 
 
-  public function createValidator($messageFile, $validationFile)
-  {
-    $metadataFactory = new ClassMetadataFactory(new LoaderChain(array(
-      new AnnotationLoader(),
-      new XmlFileLoader($validationFile)
-    )));
-    $validatorFactory = new ConstraintValidatorFactory();
-    $messageInterpolator = new NoValidationXliffMessageInterpolator($messageFile);
-    $validator = new Validator($metadataFactory, $validatorFactory, $messageInterpolator);
+    public function createValidator($messageFile, $validationFile)
+    {
+        $metadataFactory = new ClassMetadataFactory(new LoaderChain(array(
+            new AnnotationLoader(),
+            new XmlFileLoader($validationFile)
+        )));
+        $validatorFactory = new ConstraintValidatorFactory();
+        $messageInterpolator = new NoValidationXliffMessageInterpolator($messageFile);
+        $validator = new Validator($metadataFactory, $validatorFactory, $messageInterpolator);
 
-    return $validator;
-  }
+        return $validator;
+    }
 }
