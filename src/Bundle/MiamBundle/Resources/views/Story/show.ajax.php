f83a769ca428b14476954b518af7ae461bea1131
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-06-07 23:42:49 +0200

    Show story creation date and user - Close #17

--- src/Bundle/MiamBundle/Resources/views/Story/show.ajax.php
+++ src/Bundle/MiamBundle/Resources/views/Story/show.ajax.php
@@ -1,11 +1,19 @@
-<div class="titleWithActions">
-    <a href="<?php echo $view->router->generate('story_edit', array('id' => $story->getId())) ?>">Modifier</a>
-    <a title="Supprimer la story" class="js_confirm"  href="<?php echo $view->router->generate('story_delete', array('id' => $story->getId())) ?>">Supprimer</a>
-    <h1 class="story_title"><?php echo $story->getName() ?></h1>
+<div class="story_title">
+    <?php echo $story->getName() ?>
 </div>
 
-<div class="story_body">
-  <?php echo $view->markdown->transform($story->getBody()) ?>
+<div class="story_body clearfix">
+    <?php echo $view->markdown->transform($story->getBody()) ?>
+    <div class="story_infos">
+        Créé le <?php echo $story->getCreatedAt()->format('d F à H\hi') ?>
+        <?php if(!empty($timeline)): ?>
+            par <?php echo $timeline[count($timeline)-1]->getUser()->getUsername() ?>
+        <?php endif; ?>
+    </div>
+    <div class="story_actions">
+        <a href="<?php echo $view->router->generate('story_edit', array('id' => $story->getId())) ?>">Modifier</a>
+        <a title="Supprimer la story" class="js_confirm"  href="<?php echo $view->router->generate('story_delete', array('id' => $story->getId())) ?>">Supprimer</a>
+    </div>
 </div>
 
 <div class="story_timeline">
