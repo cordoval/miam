045483e897f4419a5695bf7b5f85ef2166871006
ornicar <thibault.duplessis@gmail.com>
2010-06-23 17:14:31 +0200

    Make story project view incredibly beautiful.

--- src/Bundle/MiamBundle/Resources/views/Sprint/_listStories.php
+++ src/Bundle/MiamBundle/Resources/views/Sprint/_listStories.php
@@ -2,7 +2,7 @@
     <?php foreach ($stories as $story): ?>
         <li class="story story_object" id="story_<?php echo $story->getId() ?>" data-story-id="<?php echo $story->getId() ?>">
             <a class="status_<?php echo $story->renderStatus() ?>" href="<?php echo $view->router->generate('story', array('id' => $story->getId())) ?>"><?php echo $story->getName() ?></a>
-            <span class="story_project" style="background:<?php echo $story->getProject()->getColor() ?>">
+            <span class="story_project" style="border-color:<?php echo $story->getProject()->getColor() ?>">
                 <a href="<?php echo $view->router->generate('project', array('id' => $story->getProject()->getId())) ?>">
                     <?php echo $story->getProject()->getName() ?>
                 </a>
