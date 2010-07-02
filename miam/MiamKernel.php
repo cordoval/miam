21477f799edb4663296f04de385d450544061941
ornicar <thibault.duplessis@gmail.com>
2010-07-02 15:21:21 +0200

    Compat changes

--- miam/MiamKernel.php
+++ miam/MiamKernel.php
@@ -6,73 +6,79 @@ use Symfony\Foundation\Kernel;
 use Symfony\Components\DependencyInjection\Loader\YamlFileLoader as ContainerLoader;
 use Symfony\Components\Routing\Loader\YamlFileLoader as RoutingLoader;
 
+use Symfony\Foundation\KernelBundle;
+use Symfony\Framework\FoundationBundle\FoundationBundle;
+use Symfony\Framework\ZendBundle\ZendBundle;
+use Symfony\Framework\DoctrineBundle\DoctrineBundle;
+use Symfony\Framework\DoctrineMigrationsBundle\DoctrineMigrationsBundle;
+
 class MiamKernel extends Kernel
 {
-  
-  public function registerRootDir()
-  {
-    return __DIR__;
-  }
 
-  public function registerBundles()
-  {
-    $bundles = array(
-      new Symfony\Foundation\Bundle\KernelBundle(),
-      new Symfony\Framework\DoctrineBundle\DoctrineBundle(),
-      new Symfony\Framework\DoctrineMigrationsBundle\DoctrineMigrationsBundle(),
-      new Symfony\Framework\FoundationBundle\FoundationBundle(),
-      new Symfony\Framework\ZendBundle\ZendBundle(),
-      new Bundle\MarkdownBundle\MarkdownBundle(),
-      new Bundle\DoctrineUserBundle\DoctrineUserBundle(),
-      new Bundle\MiamBundle\MiamBundle()
-    );
-
-    return $bundles;
-  }
-
-  public function registerBundleDirs()
-  {
-    return array(
-      'Bundle'             => __DIR__.'/../src/Bundle',
-      'Symfony\\Framework' => __DIR__.'/../src/vendor/Symfony/src/Symfony/Framework',
-    );
-  }
-
-  /**
-   * Returns the config_{environment}_local.yml file or 
-   * the default config_{environment}.yml if it does not exist.
-   * Useful to override development password.
-   *
-   * @param string Environment
-   * @return The configuration file path
-   */
-  protected function getLocalConfigurationFile($environment)
-  {
-    $basePath = __DIR__.'/config/config_';
-    $file = $basePath.$environment.'_local.yml';
-    
-    if(\file_exists($file))
+    public function registerRootDir()
     {
-      return $file;
+        return __DIR__;
     }
-    
-    return $basePath.$environment.'.yml';
-  }
-  
-  public function registerContainerConfiguration()
-  {
-    $loader = new ContainerLoader($this->getBundleDirs());
 
-    $configuration = $loader->load($this->getLocalConfigurationFile($this->getEnvironment()));
-    $configuration->merge($loader->load(__DIR__.'/config/dic_'.$this->getEnvironment().'.yml'));
+    public function registerBundles()
+    {
+        $bundles = array(
+            new KernelBundle(),
+            new FoundationBundle(),
+            new DoctrineBundle(),
+            new DoctrineMigrationsBundle(),
+            new ZendBundle(),
+            new Bundle\MarkdownBundle\MarkdownBundle(),
+            new Bundle\DoctrineUserBundle\DoctrineUserBundle(),
+            new Bundle\MiamBundle\MiamBundle()
+        );
 
-    return $configuration;
-  }
+        return $bundles;
+    }
 
-  public function registerRoutes()
-  {
-    $loader = new RoutingLoader($this->getBundleDirs());
+    public function registerBundleDirs()
+    {
+        return array(
+            'Bundle'             => __DIR__.'/../src/Bundle',
+            'Symfony\\Framework' => __DIR__.'/../src/vendor/Symfony/src/Symfony/Framework',
+        );
+    }
 
-    return $loader->load(__DIR__.'/config/routing.yml');
-  }
+    /**
+     * Returns the config_{environment}_local.yml file or 
+     * the default config_{environment}.yml if it does not exist.
+     * Useful to override development password.
+     *
+     * @param string Environment
+     * @return The configuration file path
+     */
+    protected function getLocalConfigurationFile($environment)
+    {
+        $basePath = __DIR__.'/config/config_';
+        $file = $basePath.$environment.'_local.yml';
+
+        if(\file_exists($file))
+        {
+            return $file;
+        }
+
+        return $basePath.$environment.'.yml';
+    }
+
+    public function registerContainerConfiguration()
+    {
+        $loader = new ContainerLoader($this->getBundleDirs());
+
+        $configuration = $loader->load($this->getLocalConfigurationFile($this->getEnvironment()));
+        $configuration->merge($loader->load(__DIR__.'/config/dic_'.$this->getEnvironment().'.yml'));
+
+        return $configuration;
+    }
+
+    public function registerRoutes()
+    {
+        $loader = new RoutingLoader($this->getBundleDirs());
+
+        return $loader->load(__DIR__.'/config/routing.yml');
+    }
 }
