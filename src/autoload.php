07b0ede55616269f422605a6f5007141e80c6ec5
ornicar <thibault.duplessis@gmail.com>
2010-07-12 17:46:33 +0200

    Fix autoload

--- src/autoload.php
+++ src/autoload.php
@@ -1,21 +1,17 @@
 <?php
 
-require_once __DIR__.'/vendor/Symfony/src/Symfony/Foundation/UniversalClassLoader.php';
+require_once __DIR__.'/vendor/Symfony/src/Symfony/Framework/UniversalClassLoader.php';
 
-use Symfony\Foundation\UniversalClassLoader;
-
-$loader = new UniversalClassLoader();
+$loader = new Symfony\Framework\UniversalClassLoader();
 $loader->registerNamespaces(array(
-  'Symfony'     => __DIR__.'/vendor/Symfony/src',
-  'Bundle'      => __DIR__,
-  'Doctrine\DBAL\Migrations' => __DIR__.'/vendor/migrations/lib',
-  'Doctrine\DBAL'    => __DIR__.'/vendor/doctrine/lib/vendor/doctrine-dbal/lib',
-  'Doctrine\Common'    => __DIR__.'/vendor/doctrine/lib/vendor/doctrine-common/lib',
-  'Doctrine'    => __DIR__.'/vendor/doctrine/lib',
-  'Zend'    => __DIR__.'/vendor/zend/library',
+    'Symfony'                   => __DIR__.'/vendor/Symfony/src',
+    'Application'               => __DIR__,
+    'Bundle'                    => __DIR__,
+    'Doctrine\DBAL\Migrations'  => __DIR__.'/vendor/DoctrineMigrations/lib',
+    'Doctrine\Common'           => __DIR__.'/vendor/Doctrine/lib/vendor/doctrine-common/lib',
+    'Doctrine\DBAL'             => __DIR__.'/vendor/Doctrine/lib/vendor/doctrine-dbal/lib',
+    'Doctrine'                  => __DIR__.'/vendor/Doctrine/lib',
+    'Zend'                      => __DIR__.'/vendor/Zend/library',
 ));
-$loader->register();
 
-// Include symfony 1 stuff like sfForm
-require_once __DIR__ . '/vendor/symfony1/autoload/sfCoreAutoload.class.php';
-sfCoreAutoload::register();
+$loader->register();
