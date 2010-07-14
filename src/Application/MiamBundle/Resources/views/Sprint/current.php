643e5ffd5d95ce5ebcc49f95ed3c77dba8e82936
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-07-14 21:54:44 +0200

    Move the sidebar to the let

--- src/Application/MiamBundle/Resources/views/Sprint/current.php
+++ src/Application/MiamBundle/Resources/views/Sprint/current.php
@@ -3,12 +3,12 @@
 <?php $view->slots->set('active_menu', 'sprint_current') ?>
 
 
-<div id="timeline" class="colRight">
+<div id="timeline" class="colSide">
   <div id="timeline_close">X</div>
   <?php $view->output('MiamBundle:Timeline:list', array('timeline' => $timeline, 'emails' => $emails)); ?>
 </div>
 
-<div id="sprint_current" class="colLeft">
+<div id="sprint_current" class="colCenter">
   <?php $view->output('MiamBundle:Sprint:_current', array('sprint' => $sprint, 'hash' => $hash, 'projects' => $projects, 'statuses' => $statuses)); ?>    
 </div>
 
