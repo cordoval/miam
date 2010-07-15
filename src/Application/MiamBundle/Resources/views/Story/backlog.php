c79fae43d29bd5873bb0c7599848979ef1353a4f
ornicar <thibault.duplessis@gmail.com>
2010-07-15 17:13:09 +0200

    Add backlog view

--- src/Application/MiamBundle/Resources/views/Story/backlog.php
+++ src/Application/MiamBundle/Resources/views/Story/backlog.php
@@ -1,9 +1,17 @@
-<ol class="stories">
-    <?php foreach ($stories as $story): ?>
-        <li class="story story_object" id="story_<?php echo $story->getId() ?>" data-story-id="<?php echo $story->getId() ?>">
-            <a href="<?php echo $view->router->generate('story', array('id' => $story->getId())) ?>"><?php echo $story->getName() ?></a>
-            <span class="story_project" style="background:<?php echo $story->getProject()->getColor() ?>"><a href="<?php echo $view->router->generate('project', array('id' => $story->getProject()->getId())) ?>"><?php echo $story->getProject()->getName() ?></a></span>
-            <span class="story_points"><?php echo $story->getPoints() ? $story->getPoints() : '?' ?></span>
-        </li>
-    <?php endforeach ?>
-</ol>
+<div class="projects">
+<?php foreach($sections as $section): ?>
+    <div class="project project_<?php echo $section['project']->getId() ?>" data-project-id="<?php echo $section['project']->getId() ?>">
+        <div class="project_name" style="background: <?php echo $section['project']->getColor() ?>">
+            <?php echo $section['project']->getName() ?>
+        </div>
+        <ul class="stories" data-project-id="<?php echo $section['project']->getId() ?>">
+        <?php foreach ($section['stories'] as $story): ?>
+            <li class="story story_object story_<?php echo $story->getId() ?>" rel="story_<?php echo $story->getId() ?>" data-story-id="<?php echo $story->getId() ?>">
+                <a href="<?php echo $view->router->generate('story', array('id' => $story->getId())) ?>"><?php echo $story->getName() ?></a>
+                <span class="story_points"><?php echo $story->getPoints() ? $story->getPoints() : '?' ?></span>
+            </li>
+        <?php endforeach ?>
+        </ul>
+    </div>
+<?php endforeach ?>
+</div>
