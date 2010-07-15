b742d3ced83c21307addfa6993c797c69e530c63
ornicar <thibault.duplessis@gmail.com>
2010-07-15 17:11:20 +0200

    Remove migrations and add a new one

--- src/Application/MiamBundle/DoctrineMigrations/Version20100715140737.php
+++ src/Application/MiamBundle/DoctrineMigrations/Version20100715140737.php
@@ -5,15 +5,15 @@ namespace Application\MiamBundle\DoctrineMigrations;
 use Doctrine\DBAL\Migrations\AbstractMigration,
     Doctrine\DBAL\Schema\Schema;
 
-class Version20100531160518 extends AbstractMigration
+class Version20100715140737 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        $this->_addSql('ALTER TABLE miam_story ADD updated_at DATETIME NOT NULL');
+        $this->_addSql('ALTER TABLE miam_project ADD priority INT NOT NULL');
     }
 
     public function down(Schema $schema)
     {
-        $this->_addSql('ALTER TABLE miam_story DROP updated_at');
+        $this->_addSql('ALTER TABLE miam_project DROP priority');
     }
 }
